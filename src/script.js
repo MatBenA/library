2e6d2bca9e4e7a8c4e0b0f2a73a164b0ecac66b9
Matías <crosserain@gmail.com>
2024-02-26 11:45:30 -0300

    added function that displays an element

--- src/script.js
+++ src/script.js
@@ -1,47 +1,67 @@
+//Books storage
 const myLibrary = [];
 
 myLibrary.push(new Books("1984", "George Orwell", 328, false));
-
-console.log(myLibrary);
-
-displayBook();
 
 function Books(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-
-    /*     this.info = () => {
-        return `${this.title} by ${author}, ${this.pages} pages, ${
-            isRead ? "already read" : "not read yet"
-        } `;
-    }; */
 }
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+//TODO loops through the array and displays each book on the page
 function displayLibrary() {}
 
-function displayBook() {
-    const myBook = myLibrary[0];
-    
-    const library = document.querySelector("section");
-    
-    const bookCard = document.createElement("div");
-    library.appendChild(bookCard);
-    bookCard.setAttribute("class", "book-card");
+//TODO function that renders a single book
+function displayBook() {}
 
-    
+/*  
+    TODO Add a “NEW BOOK” button that brings up a form allowing 
+    users to input the details for the new book: author, title, 
+    number of pages, whether it’s been read 
+*/
+
+const library = document.querySelector(".library");
+const card = document.createElement("div");
+library.appendChild(card);
+card.setAttribute("class", "book-card");
+
+displayElement(".book-card", "h3", "1984");
+/* const title = document.createElement("h3");
+title.innerText = "1984";
+card.appendChild(title); */
+
+displayElement(".book-card", "div", "George Orwell");
+/* const author = document.createElement("div");
+author.innerText = "George Orwell";
+card.appendChild(author); */
+
+displayElement(".book-card", "div", 328);
+/* const pages = document.createElement("div");
+pages.innerText = 328;
+card.appendChild(pages); */
+
+displayElement(".book-card", "button", "Completed", "completed");
+/* const isread = document.createElement("button");
+isread.innerText = "Completed";
+card.appendChild(isread);
+isread.setAttribute("class", "completed"); */
+
+displayElement(".book-card", "button", "Remove", "remove")
+/* const remove = document.createElement("button");
+card.appendChild(remove);
+remove.setAttribute("class", "remove");
+remove.innerText = "Remove"; */
+
+function displayElement(parentSelector, tagName, innerText, className) {
+    const parent = document.querySelector(`${parentSelector}`);
+    const element = document.createElement(`${tagName}`);
+    parent.appendChild(element);
+    className && element.setAttribute("class", `${className}`);
+    innerText && (element.innerText = `${innerText}`);
 }
-
-//const Book1 = new Books("1984", "George Orwell", 328, false);
-
-// console.log(book1.info());
-/* console.log(Books.prototype);
-console.log(Object.getPrototypeOf(Book1));
-console.log(Object.getPrototypeOf(Book1) === Books.prototype); */
-
-//Object.prototype.valueOf();
