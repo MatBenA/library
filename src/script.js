6aebc46e51b1271a485922a397b9ba1994215a61
Matías <crosserain@gmail.com>
2024-02-29 10:28:49 -0300

    remove buttons working

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 //TODO add functionality to the remove card button
 //Books storage
-const myLibrary = [];
+let myLibrary = [];
 
 myLibrary.push(new Books("1984", "George Orwell", 328, false));
 myLibrary.push(new Books("El Imperio Final", "Brandon Sanderson", 688, false));
@@ -19,37 +19,39 @@ function Books(title, author, pages, isRead) {
 }
 
 //TODO make card button toggle between Completed and On Read States
-Books.prototype.toggleRead = () => {
-
-}
+Books.prototype.toggleRead = () => {};
 
 //loops through the array and displays each book on the page
 function displayLibrary(myLibrary) {
-    myLibrary.forEach((book) => {
-        displayBook(book); //book.display(); refactor
+    myLibrary.forEach((book, i) => {
+        displayBook(book, i); //book.display(); refactor
     });
 }
 
 //function that renders a single book
-function displayBook({ title, author, pages, isRead }) {
+function displayBook({ title, author, pages, isRead }, index) {
     const library = document.querySelector(".library");
     const card = displayElement(library, "div", "", "book-card");
     displayElement(card, "h3", title);
     displayElement(card, "div", `Author: ${author}`);
     displayElement(card, "div", `Pages: ${pages}`);
     displayElement(card, "button", "Completed", "green-btn");
-    displayElement(card, "button", "Remove", "red-btn");
+    displayElement(card, "button", "Remove", "red-btn remove", index);
 }
 
 //creates an html element given its parent, tag, text and class, return created element
-function displayElement(parent, tagName, innerText, className) {
+function displayElement(parent, tagName, innerText, className, index) {
     const element = document.createElement(`${tagName}`);
     parent.appendChild(element);
     className && element.setAttribute("class", `${className}`);
     innerText && (element.innerText = `${innerText}`);
+
+    
+    if (innerText === "Remove") {
+        setRemoveBtn(element, index);
+    }
     return element;
 }
-
 
 //Modal Management
 const showBtn = document.querySelector(".add-icon");
@@ -84,4 +86,16 @@ function reRenderLibrary() {
     library.setAttribute("class", "library");
     container.appendChild(library);
     displayLibrary(myLibrary);
+}
+
+//sets remove button for book cards
+function setRemoveBtn(button, index) {
+    button.setAttribute("data-index", index);
+    //each time a card is removed from myLibrary the layout is re rendered
+    button.addEventListener("click", ({ target }) => {
+        myLibrary = myLibrary.filter(
+            (book, index) => index !== parseInt(target.dataset.index)
+        );
+        reRenderLibrary();
+    });
 }
